1989091333b3ab5cb00431f69225a27f6de14373
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-06-03 20:57:52 +0530

    added hamburger menu to header for smaller screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 // import Logo from "../../public/assets/Logo.svg";
+import { useState } from "react";
 import Logo from "../assets/Logo.svg";
 import Nav from "./Nav";
 
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <header className="header row row-center">
+      <button className="hamburger" onClick={() => setShowNav(!showNav)}>
+        Ξ
+      </button>
       <img src={Logo} alt="Little Lemon Logo" />
-      <Nav />
+      {showNav && <Nav />}
     </header>
   );
 }
