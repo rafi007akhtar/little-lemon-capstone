6acc9e0e520220bfbbfcff3fb4604b9966573423
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-06-03 21:00:45 +0530

    Implemented the BookingForm component - added the required fields for reserving a table - implemented the logic and view in the js file - implemented the styling in App.css file - commented out other parts of the component in App.js and showing only   booking form for now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import BookingForm from "./components/BookingForm";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Main />
-      <Footer />
+      {/* <Main />
+      <Footer /> */}
+      <BookingForm />
     </div>
   );
 }
---
+++ src/components/BookingForm.js
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import FlatButton from "./common-components/FlatButton";
+
+function getTodaysDate() {
+  const date = new Date();
+  let dd = date.getDate();
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  let mm = date.getMonth() + 1;
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+export default function BookingForm() {
+  const [date, setDate] = useState(getTodaysDate());
+  const [time, setTime] = useState("");
+  const [occasion, setOccasion] = useState("");
+  const [dinerCount, setDinerCount] = useState(1);
+  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+
+  const bookedInfo = {
+    date: "",
+    time: "",
+    occasion: "",
+    dinerCount: 0,
+  };
+
+  // let formIsSubmitted = false;
+
+  function decreaseDinerCount() {
+    if (dinerCount <= 1) return;
+    setDinerCount((state) => state - 1);
+  }
+
+  function increaseDinerCount() {
+    if (dinerCount >= 4) return;
+    setDinerCount((state) => state + 1);
+  }
+
+  function resetStates() {
+    setDate(getTodaysDate());
+    setTime("");
+    setOccasion("");
+    setDinerCount(1);
+  }
+
+  function formSubmit(e) {
+    e.preventDefault();
+    // formIsSubmitted = true;
+    setFormIsSubmitted(true);
+    bookedInfo.date = date;
+    bookedInfo.time = time;
+    bookedInfo.occasion = occasion;
+    bookedInfo.dinerCount = dinerCount;
+    console.log(
+      "The resevation has been booked with these details:",
+      bookedInfo
+    );
+    resetStates();
+  }
+
+  function formChange() {
+    // formIsSubmitted = false;
+    setFormIsSubmitted(false);
+  }
+
+  return (
+    <div className="table-booking">
+      <h1>Reservation Details</h1>
+      <p>Please enter the following details needed for the reservation.</p>
+
+      <hr />
+
+      <form onSubmit={formSubmit} onChange={formChange}>
+        <label htmlFor="reservation-date" className="row">
+          Reservation Date
+        </label>
+        <input
+          type="date"
+          name="reservation-date"
+          id="reservation-date"
+          className="reservation-field"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+
+        <hr />
+
+        <label htmlFor="reservation-time" className="row">
+          Reservation time
+        </label>
+        <input
+          type="time"
+          name="reservation-time"
+          id="reservation-time"
+          className="reservation-field"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+
+        <hr />
+
+        <label htmlFor="reservation-occasion" className="row">
+          Occasion
+        </label>
+        <input
+          type="text"
+          name="reservation-occasion"
+          id="reservation-occasion"
+          className="reservation-field occasion"
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+        />
+
+        <hr />
+
+        <label htmlFor="resevation-diners" className="row">
+          Number of diners
+        </label>
+        <div className="diner-count">
+          <FlatButton onClick={decreaseDinerCount}>-</FlatButton>
+          <span id="resevation-diners"> {dinerCount} </span>
+          <FlatButton onClick={increaseDinerCount}>+</FlatButton>
+        </div>
+
+        <div className="form-submission">
+          <button
+            className={`form-submit button ${
+              formIsSubmitted ? "submitted" : ""
+            }`}
+          >
+            {formIsSubmitted ? "Submitted" : "Submit"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
