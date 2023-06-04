921f16911cc7ad1769d1dcec5501d1095f0b7233
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-06-04 12:37:43 +0530

    added availableTimes state in App.js and made it use a reducer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import BookingForm from "./components/BookingForm";
+import { useReducer } from "react";
+
+function updateTimes(state, action) {
+  switch (action.type) {
+  }
+  return state;
+}
+
+function initializeTimes() {
+  return ["9PM"];
+}
 
 function App() {
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
   return (
     <div className="container">
       <Header />
       {/* <Main />
       <Footer /> */}
-      <BookingForm />
+      <BookingForm availableTimes={{ availableTimes, dispatch }} />
     </div>
   );
 }
